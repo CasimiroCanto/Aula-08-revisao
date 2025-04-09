bea3a1ef4d74bbb1a0127d52a3c64da5c9dbea9d
CASIMIRO CAMARA CANTO <canto1951@gmail.com>
2025-04-09 19:43:04 -0300

    Add files via upload

---
+++ exercicio A09 2.1.c
@@ -0,0 +1,26 @@
+
+int multi(int);
+
+int main(void) {
+  int a = 10;
+  int b = 20;
+
+  printf("\nResultado: b*b - a*a = %d", operacao(a,b));
+  printf("\nMain: a: %d b: %d\n", a, b);
+  return 0;
+}
+
+int operacao(int x, int y) {
+  int resultado = multi(y) - multi(x);
+  x = 0;
+  y = 0;
+  printf("\nOperacao x: %d y: %d", x, y);
+  return resultado;
+}
+
+int multi(int z) {
+  int resultado = z*z;
+  z = 0;
+  printf("\nMulti: z: %d\n", z);    
+  return resultado;
+}
